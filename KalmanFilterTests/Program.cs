6b7e0687fa5ac61821bf1b134b1eaa9217af91d5
Peter H <wappospypuppy@gmail.com>
2021-07-13 00:59:41 -0700

    Started. Next, figure out what covariance means as an n x n matrix.
    Also, make test work.

--- KalmanFilterTests/Program.cs
+++ KalmanFilterTests/Program.cs
@@ -1,12 +1,83 @@
-﻿using System;
+﻿using KalmanFilters;
+
+using MathNet.Numerics.Distributions;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+using System;
+using System.Diagnostics;
 
 namespace KalmanFilterTests
 {
     class Program
     {
+        static Normal normalDistribution;
+        static KalmanFilter filter;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double deltaT = 0.05;
+            double[] stateTransition = new[]
+            {
+                1,      0,      0,
+                deltaT, 1,      0,
+                1/2 * deltaT * deltaT,  deltaT, 1
+            };
+
+            double[] processNoise = new[] { 0.1, 0.3, 0.5 };
+
+            double[] initialState = new[] { 0, 0, 0.0 };
+
+            double[] initialCovariance = new[] { 100.0, 100, 100 };
+
+            double measurementCovariance = 100;
+
+            double[] observationModel = new double[]
+            {
+                0, 0, 1
+            };
+
+            double[] controlModel = new double[]
+            {
+                0,
+                0,
+                deltaT
+            };
+
+            // So
+            // I thought covariance was just gonna be a vector.
+            // Turns out it's an n x n matrix.
+            // Why??
+            // Figure this out next.
+
+            filter = new KalmanFilter((Matrix)new DenseMatrix(3, 3, stateTransition).Transpose(), DenseMatrix.Create(3, 3, 0.01), DenseMatrix.Create(1, 1, measurementCovariance), (Matrix)new DenseMatrix(3, 1, observationModel).Transpose(), (Matrix)new DenseMatrix(1, 3, controlModel).Transpose(), new DenseVector(initialState), DenseMatrix.Create(3, 3, 100));
+
+            normalDistribution = new Normal(0, Math.Sqrt(measurementCovariance));
+
+            double position = 0;
+            double velocity = 5;
+            double acceleration = 0;
+
+            Stopwatch watch = new Stopwatch();
+
+            watch.Start();
+
+            while (true)
+            {
+                position += velocity * deltaT;
+                position += 1 / 2 * deltaT * deltaT * acceleration;
+
+                filter.Predict();
+
+                double reading = position + normalDistribution.Sample();
+                filter.Update(new DenseVector(new double[] { reading }));
+                Console.WriteLine($"Pos: {position:0.00}\t\tEstimate: {filter.StateEstimate[2]:0.00} {filter.StateEstimate[1]:0.00} {filter.StateEstimate[0]:0.00}\t\tReading: {reading}");
+
+                while (watch.Elapsed.TotalSeconds < deltaT)
+                {
+
+                }
+                watch.Restart();
+            }
         }
     }
 }
---
+++ KalmanFilters/KalmanFilter.cs
@@ -0,0 +1,60 @@
+﻿using MathNet.Numerics.LinearAlgebra.Double;
+
+using System;
+
+namespace KalmanFilters
+{
+    public class KalmanFilter
+    {
+        public Matrix StateTransitionModel;
+        public Matrix ObservationModel;
+        public Matrix ProcessNoise;
+        public Matrix ControlInputModel;
+        public Matrix MeasurementCovariance;
+
+        public Vector StateEstimate;
+        public Matrix EstimateCovariance;
+
+        private Vector aPrioriState;
+        private Matrix aPrioriCovariance;
+        
+        public KalmanFilter(Matrix stateTransition, Matrix processNoiseCovariance, Matrix measurementCovariance, Matrix observationModel, Matrix controlInputModel, Vector stateEstimate, Matrix estimateCovariance)
+        {
+            StateTransitionModel = stateTransition;
+            ProcessNoise = processNoiseCovariance;
+            ObservationModel = observationModel;
+            ControlInputModel = controlInputModel;
+            MeasurementCovariance = measurementCovariance;
+            StateEstimate = stateEstimate;
+            EstimateCovariance = estimateCovariance;
+        }
+
+        public void Predict(Vector control)
+        {
+            aPrioriState = (Vector)(StateTransitionModel * StateEstimate + ControlInputModel * control);
+            aPrioriCovariance = (Matrix)(StateTransitionModel * EstimateCovariance * StateTransitionModel.Transpose() + ProcessNoise);
+        }
+
+        public void Predict()
+        {
+            aPrioriState = (Vector)(StateTransitionModel * StateEstimate);
+            aPrioriCovariance = (Matrix)(StateTransitionModel * EstimateCovariance * StateTransitionModel.Transpose() + ProcessNoise);
+        }
+
+        public void Update(Vector measurements)
+        {
+            var residual = measurements - ObservationModel * aPrioriState;
+
+            var a = ObservationModel * aPrioriCovariance;
+            var b = a * ObservationModel.Transpose();
+
+            var covariance = b + MeasurementCovariance;// ObservationModel * aPrioriCovariance * ObservationModel.Transpose() + MeasurementCovariance;
+
+            var kalmanGain = aPrioriCovariance * ObservationModel.Transpose() * covariance.Inverse();
+
+            StateEstimate = (Vector)(aPrioriState + kalmanGain * residual);
+
+            EstimateCovariance = (Matrix)(aPrioriCovariance - kalmanGain * ObservationModel * aPrioriCovariance);
+        }
+    }
+}
