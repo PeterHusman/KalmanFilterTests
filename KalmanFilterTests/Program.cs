e9ef4f46ce3049ad5ee370da7e96bbafad27ec4f
Peter H <wappospypuppy@gmail.com>
2021-07-17 00:44:54 -0700

    Added capability for other sets of sigma points and tried playing around with process noise covariances.

--- KalmanFilterTests/Program.cs
+++ KalmanFilterTests/Program.cs
@@ -16,15 +16,6 @@ namespace KalmanFilterTests
 
         static void Main(string[] args)
         {
-            var sigmas = UnscentedKalmanFilter.GetSigmaPoints(new DenseVector(new double[] { 1 }), new DenseMatrix(1, 1, new double[] { 2 }), 1);
-            for (int i = 0; i < sigmas.Length; i++)
-            {
-                sigmas[i].sigmaPoint = (Vector<double>)new DenseVector(new double[] { sigmas[i].sigmaPoint[0] * sigmas[i].sigmaPoint[0] });
-            }
-            var transformed = UnscentedKalmanFilter.UnscentedTransform(sigmas, new DenseMatrix(1));
-
-
-            Console.ReadLine();
             double deltaT = 0.05;
             double[] stateTransition = new[]
             {
@@ -32,6 +23,32 @@ namespace KalmanFilterTests
                 deltaT, 1,      0,
                 0.5 * deltaT * deltaT,  deltaT, 1
             };
+
+#if true
+            Matrix covMat = new DenseMatrix(2, 2, new double[] { 2, 1, 1, 3 });
+            // a[0] = 1 * a[0] + 0 * a[1]
+            // a[1] = 0.5 * a[0] + 1 * a[1]
+            Matrix sta = (Matrix)new DenseMatrix(2, 2, new double[] { 1, 0.5, 0, 1 });
+
+            Vector val = new DenseVector(new double[] { 1, 0 });
+
+            var newCovMat = sta * covMat * sta.Transpose();
+            var newMean = sta * val;
+
+            Func<Vector, Matrix, (Vector<double> sigmaPoint, double weight_m, double weight_c)[]> getSigs;
+            //getSigs = UnscentedKalmanFilter.GetGetSigmaPointsMethodOne(1e-3, 2, 0);
+            getSigs = UnscentedKalmanFilter.GetGetSigmaPointsMethodTwo(1);
+
+            var sigmas = getSigs(val, /*new DenseMatrix(2, 2, new double[] { 2, 1, 1, 3 })*/covMat);
+            for (int i = 0; i < sigmas.Length; i++)
+            {
+                sigmas[i].sigmaPoint = sta * sigmas[i].sigmaPoint;// (Vector<double>)new DenseVector(new double[] { sigmas[i].sigmaPoint[0], sigmas[i].sigmaPoint[1] + sigmas[i].sigmaPoint[0] * 0.5 });
+            }
+            var transformed = UnscentedKalmanFilter.UnscentedTransform(sigmas, new DenseMatrix(2));
+#endif
+
+            Console.ReadLine();
+            
 
             double[] processNoise = new[] { 0.1, 0.3, 0.5 };
 
@@ -55,9 +72,9 @@ namespace KalmanFilterTests
 
             double[] processCovariance = new double[]
             {
-                0.1, 0, 1,
-                0, 1, 1,
-                1, 1, 4
+                10, 0, 1,
+                0, 100, 1,
+                1, 1, 400
             };
 
             // What does 'positive definite' mean?
@@ -83,9 +100,9 @@ namespace KalmanFilterTests
             // What does this do?
             double kappa = 1;
 
-            filter = new KalmanFilter((Matrix)new DenseMatrix(3, 3, stateTransition).Transpose(), (Matrix)new DenseMatrix(3, 3, processCovariance).Transpose(), DenseMatrix.Create(1, 1, measurementCovariance), (Matrix)new DenseMatrix(3, 1, observationModel).Transpose(), (Matrix)new DenseMatrix(1, 3, controlModel).Transpose(), new DenseVector(initialState), DenseMatrix.Create(3, 3, 100));
+            //filter = new KalmanFilter((Matrix)new DenseMatrix(3, 3, stateTransition).Transpose(), (Matrix)new DenseMatrix(3, 3, processCovariance).Transpose(), DenseMatrix.Create(1, 1, measurementCovariance), (Matrix)new DenseMatrix(3, 1, observationModel).Transpose(), (Matrix)new DenseMatrix(1, 3, controlModel).Transpose(), new DenseVector(initialState), (Matrix)new DenseMatrix(3, 3, initialCovariance).Transpose()/*DenseMatrix.Create(3, 3, 100)*/);
 
-            //filter = new UnscentedKalmanFilter(a => new DenseVector(new[] { a[0], a[1] + deltaT * a[0], a[2] + deltaT * a[1] + 0.5 * deltaT * deltaT * a[0] }), (Matrix)new DenseMatrix(3, 3, processCovariance).Transpose(), a => new DenseVector(new[] { a[2] }), DenseMatrix.Create(1, 1, measurementCovariance), new DenseVector(initialState), (Matrix)new DenseMatrix(3, 3, initialCovariance).Transpose(), kappa);
+            filter = new UnscentedKalmanFilter(a => new DenseVector(new[] { a[0], a[1] + deltaT * a[0], a[2] + deltaT * a[1] + 0.5 * deltaT * deltaT * a[0] }), (Matrix)new DenseMatrix(3, 3, processCovariance).Transpose(), a => new DenseVector(new[] { a[2] }), DenseMatrix.Create(1, 1, measurementCovariance), new DenseVector(initialState), (Matrix)new DenseMatrix(3, 3, initialCovariance).Transpose(), getSigs);
 
             normalDistribution = new Normal(0, Math.Sqrt(measurementCovariance));
 
--- KalmanFilters/UnscentedKalmanFilter.cs
+++ KalmanFilters/UnscentedKalmanFilter.cs
@@ -16,16 +16,16 @@ namespace KalmanFilters
         public Matrix MeasurementCovariance;
         public Vector StateEstimate { get; private set; }
         public Matrix EstimateCovariance { get; private set; }
-        private (Vector<double> sigmaPoint, double weight)[] sigmas;
+        private (Vector<double> sigmaPoint, double weight_mean, double weight_covariance)[] sigmas;
 
         private Vector<double> predictedMeasurements;
         private Matrix predictedMeasurementCovariance;
 
         private Matrix<double> kalmanGain;
 
-        public double Kappa;
+        public Func<Vector, Matrix, (Vector<double>, double, double)[]> GetSigmas;
 
-        public UnscentedKalmanFilter(Func<Vector, Vector> stateTransitionModel, Matrix processNoiseCovariance, Func<Vector, Vector> observationModel, Matrix measurementNoiseCovariance, Vector stateEstimate, Matrix estimateCovariance, double kappa)
+        public UnscentedKalmanFilter(Func<Vector, Vector> stateTransitionModel, Matrix processNoiseCovariance, Func<Vector, Vector> observationModel, Matrix measurementNoiseCovariance, Vector stateEstimate, Matrix estimateCovariance, Func<Vector, Matrix, (Vector<double>, double, double)[]> getSigmas)
         {
             StateTransitionModel = stateTransitionModel;
             ObservationModel = observationModel;
@@ -33,16 +33,16 @@ namespace KalmanFilters
             EstimateCovariance = estimateCovariance;
             ProcessNoiseCovariance = processNoiseCovariance;
             MeasurementCovariance = measurementNoiseCovariance;
-            Kappa = kappa;
+            GetSigmas = getSigmas;
         }
 
         public void Predict()
         {
-            sigmas = GetSigmaPoints(StateEstimate, EstimateCovariance, Kappa);
-            var sigmaCopy = new (Vector<double>, double)[sigmas.Length];
+            sigmas = GetSigmas(StateEstimate, EstimateCovariance);
+            var sigmaCopy = new (Vector<double>, double, double)[sigmas.Length];
             for (int i = 0; i < sigmas.Length; i++)
             {
-                sigmaCopy[i] = (StateTransitionModel((Vector)sigmas[i].sigmaPoint), sigmas[i].weight);
+                sigmaCopy[i] = (StateTransitionModel((Vector)sigmas[i].sigmaPoint), sigmas[i].weight_mean, sigmas[i].weight_covariance);
             }
             // THIS STEP is to blame.
             // Why??
@@ -62,17 +62,17 @@ namespace KalmanFilters
             // Maybe put this here? I'm not 100%
             // sigmas = GetSigmaPoints(StateEstimate, EstimateCovariance, Kappa);
 
-            var sigmaCopy2 = new (Vector<double>, double)[sigmas.Length];
+            var sigmaCopy2 = new (Vector<double>, double, double)[sigmas.Length];
             for (int i = 0; i < sigmas.Length; i++)
             {
-                sigmaCopy2[i] = (ObservationModel((Vector)sigmas[i].sigmaPoint), sigmas[i].weight);
+                sigmaCopy2[i] = (ObservationModel((Vector)sigmas[i].sigmaPoint), sigmas[i].weight_mean, sigmas[i].weight_covariance);
             }
             (predictedMeasurements, predictedMeasurementCovariance) = UnscentedTransform(sigmaCopy2, MeasurementCovariance);
 
             Matrix<double> stateAndPredictedMeasurementCovariance = new DenseMatrix(StateEstimate.Count, predictedMeasurements.Count);
             for (int i = 0; i < sigmas.Length; i++)
             {
-                stateAndPredictedMeasurementCovariance += sigmas[i].weight * ((sigmaCopy[i].Item1 - StateEstimate).ToColumnMatrix() /*.OuterProduct*/ * (sigmaCopy2[i].Item1 - predictedMeasurements).ToRowMatrix());
+                stateAndPredictedMeasurementCovariance += sigmas[i].weight_covariance * ((sigmaCopy[i].Item1 - StateEstimate).ToColumnMatrix() /*.OuterProduct*/ * (sigmaCopy2[i].Item1 - predictedMeasurements).ToRowMatrix());
             }
 
             kalmanGain = stateAndPredictedMeasurementCovariance * predictedMeasurementCovariance.Inverse();
@@ -84,8 +84,37 @@ namespace KalmanFilters
             EstimateCovariance = (Matrix)(EstimateCovariance - kalmanGain * predictedMeasurementCovariance * kalmanGain.Transpose());
         }
 
+        public static Func<Vector, Matrix, (Vector<double>, double, double)[]> GetGetSigmaPointsMethodOne(double alpha, double beta, double kappa) => (a, b) => GetSigmaPointsMethodOne(a, b, alpha, beta, kappa);
 
-        public static (Vector<double> sigmaPoint, double weight)[] GetSigmaPoints(Vector mean, Matrix covariance, double kappa)
+        public static (Vector<double> sigmaPoint, double weight_mean, double weight_covariance)[] GetSigmaPointsMethodOne(Vector mean, Matrix covariance, double alpha, double beta, double kappa)
+        {
+            int n = mean.Count;
+            double lambda = alpha * alpha * (n + kappa) - /*n*/kappa;
+            var chol = ((n + lambda) * covariance).Cholesky().Factor;
+
+
+            double nPlusLambda = n + lambda;
+
+            //chol.Row(i)
+            List<(Vector<double> sigma, double weight_m, double weight_c)> points = new List<(Vector<double> sigma, double weight_m, double weight_c)>(mean.Count * 2 + 1);
+
+            points.Add((mean, kappa / nPlusLambda, (kappa / nPlusLambda) + (1 - alpha * alpha + beta)));
+
+            double weight = 1 / (2 * nPlusLambda);
+
+            for (int i = 0; i < n; i++)
+            {
+                var row = chol.Row(i);
+                points.Add((mean + row, weight, weight));
+                points.Add((mean - row, weight, weight));
+            }
+
+            return points.ToArray();
+        }
+
+        public static Func<Vector, Matrix, (Vector<double>, double, double)[]> GetGetSigmaPointsMethodTwo(double kappa) => (a, b) => GetSigmaPointsMethodTwo(a, b, kappa);
+
+        public static (Vector<double> sigmaPoint, double weight_mean, double weight_covariance)[] GetSigmaPointsMethodTwo(Vector mean, Matrix covariance, double kappa)
         {
             int n = mean.Count;
             var chol = ((n + kappa) * covariance).Cholesky().Factor;
@@ -94,42 +123,43 @@ namespace KalmanFilters
             double nPlusKappa = n + kappa;
 
             //chol.Row(i)
-            List<(Vector<double> sigma, double weight)> points = new List<(Vector<double> sigma, double weight)>(mean.Count * 2 + 1);
+            List<(Vector<double> sigma, double weight_m, double weight_c)> points = new List<(Vector<double> sigma, double weight_m, double weight_c)>(mean.Count * 2 + 1);
 
-            points.Add((mean, kappa /*or is it just 1?*/ / nPlusKappa));
+            points.Add((mean, kappa / nPlusKappa, kappa / nPlusKappa));
 
             double weight = 1 / (2 * nPlusKappa);
 
             for (int i = 0; i < n; i++)
             {
                 var row = chol.Row(i);
-                points.Add((mean + row, weight));
-                points.Add((mean - row, weight));
+                points.Add((mean + row, weight, weight));
+                points.Add((mean - row, weight, weight));
             }
 
             return points.ToArray();
         }
 
-        public static (Vector mean, Matrix covariance) UnscentedTransform((Vector<double> sigmaPoint, double weight)[] sigmaPointsWithWeights, Matrix noiseCovariance)
+        public static (Vector mean, Matrix covariance) UnscentedTransform((Vector<double> sigmaPoint, double weight_m, double weight_c)[] sigmaPointsWithWeights, Matrix noiseCovariance)
         {
             Vector<double> mean = new DenseVector(sigmaPointsWithWeights[0].sigmaPoint.Count);
-            foreach ((Vector<double> sigma, double weight) in sigmaPointsWithWeights)
+            foreach ((Vector<double> sigma, double weight_m, double weight_c) in sigmaPointsWithWeights)
             {
-                mean += sigma * weight;
+                mean += sigma * weight_m;
             }
             
             Matrix<double> covariance = new DenseMatrix(noiseCovariance.RowCount);
 
-            foreach ((Vector<double> sigma, double weight) in sigmaPointsWithWeights)
+            foreach ((Vector<double> sigma, double weight_m, double weight_c) in sigmaPointsWithWeights)
             {
-                covariance += weight * (sigma - mean).ToColumnMatrix() * (sigma - mean).ToRowMatrix();
+                var diff = (sigma - mean).ToColumnMatrix();
+                covariance += weight_c * (diff * diff.Transpose());
             }
 
             // Testing
             var sum = 0.0;
             for (int i = 0; i < sigmaPointsWithWeights.Length; i++)
             {
-                sum += sigmaPointsWithWeights[i].weight;
+                sum += sigmaPointsWithWeights[i].weight_c;
             }
 
             if (sum != 1)
